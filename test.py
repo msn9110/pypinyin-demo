4127363ed6bc79f24bb384a4cb936f7880df6a59
hhs <msn9110@gmail.com>
2017-09-18 20:52:49 +0800

    init

---
+++ test.py
@@ -0,0 +1,18 @@
+from pypinyin import pinyin, lazy_pinyin, Style
+import numpy as np
+text1 = u'給我想起來，腦包我想起來來來來來來來來萊，腦包泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥'
+
+text2 = ''
+for ch in text1:
+    if '\u4e00' <= ch <='\u9fff':
+        text2 += ch
+tmp = pinyin(text2, style=Style.BOPOMOFO)  # 注音风格
+tmp = np.array(tmp).flatten()
+count = dict()
+for pronounce in tmp:
+    if pronounce in count.keys():
+        count[pronounce]+=1
+    else:
+        count[pronounce]=1
+count = sorted(count.items(), key=lambda d:d[1], reverse = True)
+print(count)
