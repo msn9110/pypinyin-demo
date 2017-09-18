57a9e11b38905cd029df751630734de472a79ce8
hhs <msn9110@gmail.com>
2017-09-18 21:46:30 +0800

    add file io

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from pypinyin import pinyin, lazy_pinyin, Style
 import numpy as np
-text1 = u'給我想起來，腦包我想起來來來來來來來來萊，腦包泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥'
+file = open('input.txt', 'r', encoding='utf-8')
+text1 = file.read()#u'給我想起來，腦包我想起來來來來來來來來萊，腦包泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥泥'
+file.close()
 
 text2 = ''
 for ch in text1:
@@ -15,4 +17,5 @@ for pronounce in tmp:
     else:
         count[pronounce]=1
 count = sorted(count.items(), key=lambda d:d[1], reverse = True)
-print(count)
+for item in count:
+    print(item)
